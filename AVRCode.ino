084918ce63a449aca269ec9f13bf27b73d591d94
Prince Gerald <pejjeeyy@gmail.com>
2026-03-16 06:10:17 +0800

    second push

--- AVRCode.ino
+++ AVRCode.ino
@@ -1,14 +1,7 @@
 #include<avr/io.h>
 
-
-
-
-
 volatile uint32_t timer0_millis = 0;
-
 uint32_t last_tick = 0;
-
-
 
 
 
@@ -18,13 +11,9 @@ uint32_t total_ticks;
 
 
 
-
-
 const uint8_t FIXED_ANGLE = 90;  
 
 float smoothAngle = 90.0;      
-
-
 
 
 
@@ -42,8 +31,6 @@ float final_ultrasonic_reading = 0;
 
 
 
- 
-
 const float target_distance = 12.415;
 
 const float kp = 1.2;  
@@ -59,18 +46,11 @@ float lastError = 0;
 float lastTime = 0;
 
 
-
-
-
-
-
 ISR(TIMER0_COMPA_vect) {
 
     timer0_millis++;
 
 }
-
-
 
 
 
@@ -89,14 +69,7 @@ void init_millis();
 uint32_t get_millis();
 
 
-
-
-
-
-
 int main(){
-
- 
 
   init_millis();
 
@@ -108,13 +81,6 @@ int main(){
 
   DDRD &= ~(1 << PD5);//echo
 
- 
-
-
-
-
-
-
 
   TCCR1A = (1 << COM1A1) | (1 << WGM11);
 
@@ -122,7 +88,6 @@ int main(){
 
   ICR1 = 39999;
 
- 
 
   setFixedAngle(FIXED_ANGLE);
 
@@ -134,44 +99,15 @@ int main(){
 
   }
 
- 
-
-
-
-
-
-
-
-
-
-
-
   while(1){
-
-
-
     uint32_t current_tick = get_millis();
-
-       
 
         if (current_tick - last_tick >= 40) {
 
              last_tick = current_tick;
 
-           
-
-       
-
-
-
               raw_distance = ultrasonic();
-
-             
-
-             
-
               Unfiltered_arr[pointer] = raw_distance;
-
               pointer = (pointer + 1) % 5;
 
               for (int i = 0; i <= N; i++) {
@@ -181,77 +117,18 @@ int main(){
               }
 
               insertionsort(filtered_arr, 5);
-
-
-
-
-
-             
-
               final_ultrasonic_reading = filtered_arr[2];
-
-             
-
-           
-
               float pid_val = calculatePID(final_ultrasonic_reading);
-
-             
-
-
-
               float targetAngle = calculateAngle(pid_val);
-
-             
-
-           
-
               smoothAngle = (smoothAngle * 0.8) + (targetAngle * 0.2);
-
-
-
               setFixedAngle((uint8_t)smoothAngle);
-
-           
-
-
-
         }
-
-
-
-
-
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 return 0;
-
-
 
 }
 
-
-
-
-
-
-
 float ultrasonic() {
-
- 
-
   PORTD &= ~(1 << PD6);
 
   for(volatile uint8_t i = 0; i < 6; i++) {}
@@ -261,19 +138,7 @@ float ultrasonic() {
   for(volatile uint8_t i = 0; i < 35; i++) {}
 
   PORTD &= ~(1 << PD6);
-
- 
-
-
-
- 
-
- 
-
      uint32_t timeout = 60000;
-
-
-
     //wait for the echoo para mo state of highhhz
 
     while (!(PIND & (1 << PIND5))) {
@@ -283,12 +148,7 @@ float ultrasonic() {
     }
 
 
-
-   
-
     start_ticks = TCNT1;
-
-
 
     //wait for the echoo para mo state of lowzz
 
@@ -300,15 +160,7 @@ float ultrasonic() {
 
     }
 
-
-
     end_ticks = TCNT1;
-
-
-
-
-
- 
 
     if (end_ticks >= start_ticks) {
 
@@ -319,7 +171,6 @@ float ultrasonic() {
         total_ticks = (40000 - start_ticks) + end_ticks;
 
     }
-
 
 
   float reading = (total_ticks * 0.5 * 0.0343) / 2.0;
@@ -335,7 +186,6 @@ float ultrasonic() {
     reading = target_distance;
 
   }
-
 
 
   return reading;
@@ -372,44 +222,22 @@ float calculatePID(float distance) {
 
 float calculateAngle(float pid_output) {
 
- 
-
  float angle = 90.0 - pid_output;
-
- 
 
 if(angle < 30)  angle = 30;
 
 if(angle > 150) angle = 150;
-
-
-
-   
-
-
-
-
-
+ 
   return angle;
 
 }
 
 
-
 void setFixedAngle(uint8_t angle) {
-
-
-
   uint16_t ticks = (uint16_t)(( (uint32_t)(angle - 30) * 4200L / 120L ) + 800L);
-
- 
-
-
-
   OCR1A = ticks;
 
 }
-
 
 
 void insertionsort(float arr[], int n) {
@@ -434,68 +262,38 @@ void insertionsort(float arr[], int n) {
 
 }
 
-
-
-
-
 void init_millis() {
 
     // I Set CTC mode Clear Timer on Compare Match
 
     TCCR0A = (1 << WGM01);
 
-
-
     // Ang Set Prescaler to  64
 
     TCCR0B = (1 << CS01) | (1 << CS00);
-
-
 
     // Set compare match value for 1ms (250 ticks)
 
      OCR0A = 249;
 
-
-
     // compare
 
     TIMSK0 |= (1 << OCIE0A);
-
-
-
-   
 
     sei();
 
 }
 
-
-
-
-
 uint32_t get_millis() {
 
     uint32_t millis_copy;
-
-   
-
-   
-
     uint8_t sreg = SREG;
 
     cli();
 
-   
-
+  
     millis_copy = timer0_millis;
 
-   
-
-     
-
     SREG = sreg;
-
-   
 
     return millis_copy;}
